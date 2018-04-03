f96c2412b74c57727064e4789cf5941e26b62a85
PankajSharma <naveen.gupta@appzlogic.com>
2018-04-03 15:23:54 +0530

    Circle Image Added

--- picker_editor/src/main/java/com/pickerandeditor/BaseActivity.java
+++ picker_editor/src/main/java/com/pickerandeditor/BaseActivity.java
@@ -20,7 +20,6 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_base);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.
                 LayoutParams.FLAG_KEEP_SCREEN_ON);
